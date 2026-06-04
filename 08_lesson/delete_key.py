a0e024632f65a121e22cf80822d2f07ea2bc0759
Khasanov Timur <Dozen-993@mail.ru>
2026-06-05 01:34:12 +0700

    Update delete_key.py

--- 08_lesson/delete_key.py
+++ 08_lesson/delete_key.py
@@ -5,8 +5,8 @@ import requests
 base_url = "https://yougile.com/"
 
 auth_param = {
-         'login' : 'dozen-993@mail.ru',
-         'password' : 'ST!2iPJ!dJPZAwZ'
+         'login' : '',
+         'password' : ''
      }
 def test_delete_key():
     resp = requests.post(base_url + "api-v2/auth/keys/get", json=auth_param)
