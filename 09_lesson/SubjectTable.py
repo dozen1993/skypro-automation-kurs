4eb2f01c71356035760ef55b8720e9c085807d4f
Khasanov Timur <dozen--993@mail.ru>
2026-06-11 23:01:49 +0700

    Девятая домашняя работа "Работа с БД в автотестах"(доработка №2)

--- 09_lesson/SubjectTable.py
+++ 09_lesson/SubjectTable.py
@@ -1,11 +1,20 @@
-from sqlalchemy import create_engine, inspect, text
+from sqlalchemy import create_engine,  text
+
 
 class SubjectTable:
     __scripts = {
-        'select':text("SELECT * FROM subject"),
-        'insert_new':text("INSERT INTO subject (subject_id, subject_title) VALUES(:new_id, :new_subject)"),
-        'update_subject':text("UPDATE subject SET subject_id=:new_sub_id WHERE subject_title=:new_subject_title"),
-        'delete_by_subject_title':text("DELETE FROM subject WHERE subject_title=:subject_to_delete")
+        'select': text("SELECT * FROM subject"),
+        'insert_new': text(
+            "INSERT INTO subject (subject_id, subject_title)"
+            " VALUES(:new_id, :new_subject)"
+        ),
+        'update_subject': text(
+            "UPDATE subject SET subject_id=:new_sub_id"
+            " WHERE subject_title=:new_subject_title"
+        ),
+        'delete_by_subject_title': text(
+            "DELETE FROM subject "
+            "WHERE subject_title=:subject_to_delete")
         }
 
     def __init__(self, connection_string):
@@ -18,21 +27,24 @@ class SubjectTable:
         conn.close()
         return rows
 
-    def create_subject(self,new_id, subject_name):
+    def create_subject(self, new_id, subject_name):
         conn = self.__db.connect()
-        conn.execute(self.__scripts['insert_new'], {'new_id': new_id, 'new_subject': subject_name})
+        conn.execute(self.__scripts['insert_new'],
+                     {'new_id':new_id, 'new_subject':subject_name})
         conn.commit()
         conn.close()
 
     def update_subject(self, new_id, title):
         conn = self.__db.connect()
-        conn.execute(self.__scripts['update_subject'], {'new_sub_id': new_id, 'new_subject_title': title})
+        conn.execute(self.__scripts['update_subject'],
+                     {'new_sub_id':new_id, 'new_subject_title':title})
         conn.commit()
         conn.close()
 
     def delete_subject(self, subject_name):
         conn = self.__db.connect()
-        conn.execute(self.__scripts['delete_by_subject_title'], {'subject_to_delete': subject_name})
+        conn.execute(self.__scripts['delete_by_subject_title'],
+                     {'subject_to_delete':subject_name})
         conn.commit()
         conn.close()
-#
+
--- 09_lesson/test_subject_database.py
+++ 09_lesson/test_subject_database.py
@@ -7,6 +7,7 @@ def test_get_subject():
     result = db.get_subjects()
     assert len(result) > 0
 
+
 def test_add_subject():
     result1 = db.get_subjects()
     name = "Зельеварение"
@@ -14,7 +15,9 @@ def test_add_subject():
     db.create_subject(new_id, name)
     result2 = db.get_subjects()
     assert result2[-1]['subject_title'] == name
-    assert len(result1)<len(result2)
+    assert len(result1) < len(result2)
+    db.delete_subject(name)
+
 
 def test_update_subject():
     name = 'Защита от злых чар'
@@ -23,13 +26,15 @@ def test_update_subject():
     db.update_subject(213, name)
     result = db.get_subjects()
     assert result[-1]['subject_id'] != new_id
+    db.delete_subject(name)
+
 
 def test_delete_subject():
-    name="Предмет на  удаление"
-    new_id= 124
+    name = "Предмет на  удаление"
+    new_id = 124
     db.create_subject(new_id, name)
     res1 = db.get_subjects()
     db.delete_subject(name)
-    res2=db.get_subjects()
-    assert len(res1)>len(res2)
+    res2 = db.get_subjects()
+    assert len(res1) > len(res2)
 #
